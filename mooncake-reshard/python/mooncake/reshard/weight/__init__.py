73f93f0e72bc2ef7e4d6c66582a52e19f2e5c64c
Vincent Gao <vincentbo@linux.alibaba.com>
2026-09-10 02:14:56 +0800

    feat(reshard): expose weight management APIs
    Signed-off-by: Vincent Gao <vincentbo@linux.alibaba.com>

--- mooncake-reshard/python/mooncake/reshard/weight/__init__.py
+++ mooncake-reshard/python/mooncake/reshard/weight/__init__.py
@@ -69,7 +69,6 @@ from .store import (
     WeightStoreWriter,
     WeightStore,
     WeightStoreError,
-    begin_managed_weight_snapshot,
 )
 
 __all__ = [
@@ -115,7 +114,6 @@ __all__ = [
     "WeightStoreWriter",
     "WeightStore",
     "WeightStoreError",
-    "begin_managed_weight_snapshot",
     "WeightUploadPlan",
     "plan_weight_upload",
     "BoundWeightFragment",
