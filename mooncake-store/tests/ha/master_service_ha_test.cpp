bd1c37c151034c1e23860472bc95731625381e23
Vincent Gao <vincentbo@linux.alibaba.com>
2026-09-10 08:47:21 +0800

    test(store): make legacy HA fixture satisfiable
    Signed-off-by: Vincent Gao <vincentbo@linux.alibaba.com>

--- mooncake-store/tests/ha/master_service_ha_test.cpp
+++ mooncake-store/tests/ha/master_service_ha_test.cpp
@@ -2753,6 +2753,10 @@ TEST_F(MasterServiceHATest, OldStandbyPromotionClearsWeightMetadata) {
         .payload_group_id = {},
         .expected_payload_count = 1,
         .expected_logical_bytes = 1024,
+        .policy = WeightStoragePolicy{
+            .preferred_residency = WeightResidencyState::HOT,
+            .migration_mode = WeightMigrationMode::MANUAL,
+        },
         .affinity_summary = WeightAffinitySummary{
             .affinity_count = 1,
             .affinity_digest = std::string(64, 'c'),
